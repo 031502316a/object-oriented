3394563754c693df2fb8a16e1d07eab902e4f052
031502316 <516567155@qq.com>
2016-05-05 23:33:40 +0800

    firstwork

---
+++ firstwork/cin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "cout.cpp"
+using namespace std;
+
+int main()
+{
+	double r;
+	cin>>r;
+	calculate(r);
+	return 0;
+}
---
+++ firstwork/cout.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+ 
+#define pi 3.1415926
+
+void calculate(double r)
+{
+    cout<<pi*r*r<<endl;
+} 
